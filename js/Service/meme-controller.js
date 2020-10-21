3853eb32d293196ebc5de2539fc09a945816bd50
Ron Goldman <rgol89@gmail.com>
2020-10-21 23:00:52 +0300

    phase 3 with bugs

--- js/Service/meme-controller.js
+++ js/Service/meme-controller.js
@@ -7,6 +7,7 @@ function onInit() {
     gCanvas = document.querySelector('#meme-canvas');
     gCtx = gCanvas.getContext('2d');
     drawImg()
+    renderCanvas()
 }
 
 function onAddText() {
@@ -17,10 +18,38 @@ function onAddText() {
 
     addTextToMeme(text);
     document.getElementById('text').value = ''
+    if (gCurrTextIdx !== 0) gCurrTextIdx++
+
+    addTextToGmeme(text)
 }
 
 //adds img from to gallery to canvas
 function onAddImgToMeme(id){
     gCurrImg = id-1
     drawImg()
+    document.querySelector('.meme-editor').style.display = 'flex'
+    renderCanvas()
+}
+
+function onFontChange(diff){
+    gFontSize += diff;
+    document.querySelector('.font-size').innerText = gFontSize;
+    changeFontSize()
+}
+
+function onMoveText(diff){
+    moveText(diff)
+
+}
+
+function onFocusText(){
+     (gMeme.lines.length <= gCurrTextIdx) ? gCurrTextIdx = 0 : gCurrTextIdx++
+    console.log(gMeme.lines[gCurrTextIdx]);
+    focusText()
+}
+
+function draw(ev){
+    console.log('X : ',ev.offsetX);
+    console.log('Y : ',ev.offsetY);
+
 }
--- js/Service/meme-model.js
+++ js/Service/meme-model.js
@@ -7,45 +7,85 @@ var gImgs = [
     { id: 3, url: 'images/3.jpg', keywords: ['happy'] }
 
 ];
-var gCurrImg = 0
-
+var gCurrImg = 0;
+var gCurrTextIdx = 0
+var gFontSize = 50;
 //current meme object
 var gMeme = {
     selectedImgId: 1,
     selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'First line',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
+    lines: [{
+        txt: '[meme here]',
+        size: gFontSize,
+        align: 'left',
+        color: 'red',
+        x: 75,
+        y: 450
+    }]
 }
 
-function createGallery(){
+function createGallery() {
 
 }
 
 //add user text to gMeme
 
-function addTextToMeme(text) {
-    gMeme.lines.push({
+function addTextToGmeme(text) {
+    gMeme.lines.unshift({
         txt: text,
-        size: 20,
+        size: 30,
         align: 'left',
-        color: 'red'
+        color: 'red',
+        x: 100,
+        y: 200
     })
-
-    drawText(text,50,50)
 }
 
+function renderCanvas() {
+    var img = new Image()
+    img.src = gImgs[gCurrImg].url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        gMeme.lines.forEach(function (line) {
+            drawText(line.txt, line.x, line.y, line.size)
+        }
+        )
+    }
+}
+
+function addTextToMeme(text) {
+    drawText(text, gMeme.lines[gCurrTextIdx].x, gMeme.lines[gCurrTextIdx].y, gMeme.lines[gCurrTextIdx].size)
+}
+
+function moveText(diff) {
+    gMeme.lines[gCurrTextIdx].y += diff
+    var img = new Image()
+    img.src = gImgs[gCurrImg].url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+
+        renderCanvas()
+
+    }
+}
+
+function changeFontSize() {
+    gMeme.lines[gCurrTextIdx].size = gFontSize
+    var img = new Image()
+    img.src = gImgs[gCurrImg].url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        renderCanvas()
+    }
+    // console.log(gMeme.lines[gCurrTextIdx].txt);
+}
 //creates text
 
-function drawText(text, x, y) {
-    gCtx.font = "40pt IMPACT";
+function drawText(text, x, y, size) {
+    gCtx.font = `${size}pt IMPACT`;
     gCtx.strokeStyle = 'black'
     gCtx.fillStyle = 'white'
+    console.log('text', gCtx.measureText(text).width);
     gCtx.fillText(text, x, y);
 }
 
@@ -56,4 +96,23 @@ function drawImg() {
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
     }
+}
+
+function clearCanvas() {
+    gCanvas = document.querySelector('#meme-canvas');
+    gCtx = gCanvas.getContext('2d');
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height)
+    drawImg()
+}
+
+function focusText(){
+
+    // drawRect(gMeme.lines[gCurrTextIdx].x, gMeme.lines[gCurrTextIdx].y, gMeme.lines[gCurrTextIdx].size)
+}
+
+function drawRect(x, y,size) {
+    gCtx.beginPath()
+    gCtx.rect(x, y, size, size)
+    gCtx.strokeStyle = 'black'
+    gCtx.stroke()
 }
